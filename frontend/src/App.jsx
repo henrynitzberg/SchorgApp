6e135180c6f5bc5c5e278599d12da81773ff36b4
henry@nitzb.org <henry@nitzb.org>
2025-04-16 23:02:50 -0400

    sidebar wip

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 import Home from "./pages/Home.jsx";
+import Layout from "./components/Layout.jsx";
 
 import "./css/index.css";
 
@@ -9,7 +10,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+export default function Sidebar() {
+    return (
+      <div className="sidebar">
+        <h2>Sidebar</h2>
+        <ul>
+          <li>Item A</li>
+          <li>Item B</li>
+          <li>Item C</li>
+        </ul>
+      </div>
+    );
+  }
