ebc6c934395814a10b1fd34812de8752b6361029
henry@nitzb.org <henry@nitzb.org>
2025-04-17 13:25:31 -0400

    add adjustable sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,13 @@
 export default function Sidebar() {
     return (
       <div className="sidebar">
-        <h2>Sidebar</h2>
+        <h2>Upcoming ToDos</h2>
+        <ul>
+          <li>Item A</li>
+          <li>Item B</li>
+          <li>Item C</li>
+        </ul>
+        <h2>Your Spaces</h2>
         <ul>
           <li>Item A</li>
           <li>Item B</li>
